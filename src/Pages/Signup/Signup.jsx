9cfa6b911f610e0934e7dd204676a96b34b6a6b9
Shahriar-Samir <shabusiness035@gmail.com>
2024-09-05 14:35:32 +0600

    add: devided sign up into two sections

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Signup = () => {
     return (
-        <div>
-            Signup
-        </div>
+        <main className='flex w-full '>
+            {/* sign up form */}
+            <section className='h-[100vh] w-1/2'>Form</section>
+            {/* title and description */}
+            <section className='h-[100vh] w-1/2'>Title</section>
+        </main>
     );
 };
 
