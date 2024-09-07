3e5a950b75525424dc465c07bf5a69cdb22844c0
Shahriar-Samir <shabusiness035@gmail.com>
2024-09-07 12:06:22 +0600

    add: order details section in cart page

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -9,11 +9,34 @@ const Cart = () => {
 
 
     return (
-        <main className='w-11/12 mx-auto mt-20 flex flex-row-reverse justify-center gap-10'>
-            <aside className='w-3/12 max-w-[500px] border-e h-[50vh] flex flex-col gap-3'>
-        
+        <main className='w-10/12 mx-auto mt-20 flex flex-row-reverse justify-center gap-10'>
+            <aside className='w-3/12 max-w-[500px] h-[50vh] flex flex-col gap-3'>
+                <h1 className='font-semibold text-black text-xl'>Order Details</h1>
+                <div className='w-full p-4 border rounded-md flex flex-col gap-2'>
+                    <div className='flex flex-col gap-3 border-b pb-3 text-gray-500'>
+                    <p className='w-full flex justify-between items-center'>
+                        <span>Subtotal</span>
+                        <span className='font-semibold'>£1071</span>
+                    </p>
+                    <p className='w-full flex justify-between items-center'>
+                        <span>Shipping</span>
+                        <span className='font-semibold'>Free</span>
+                    </p>
+                    <p className='w-full flex justify-between items-center'>
+                        <span>Estimated Tax</span>
+                        <span className='font-semibold'>£-</span>
+                    </p>
+                    </div>
+                    <p className='w-full flex justify-between items-center'>
+                        <span className='text-lg font-semibold text-gray-500'>Total</span>
+                        <span className='text-xl font-semibold'>£1071.00</span>
+                    </p>
+                </div>
+                <button className="btn bg-black text-white w-full">GO TO CHECKOUT</button>
             </aside>
-            <section className='w-9/12 grid grid-cols-1 gap-10'>
+          <section className='w-9/12'>
+            <h1 className='text-xl font-semibold'>An overview of your order</h1>
+          <div className='w-full grid grid-cols-1 gap-10'>
                 {cart?.map(item=>{
                     return <div className="card bg-base-100 shadow-xl">
                     <figure className="px-5 pt-5 w-full">
@@ -36,7 +59,8 @@ const Cart = () => {
                     </div>
                   </div>
                 })}
-            </section>
+            </div>
+          </section>
         </main>
     );
 };
