4569b114c19c64c8ae87179f296358a380059518
Shahriar-Samir <shabusiness035@gmail.com>
2024-09-07 20:52:16 +0600

    add: reponsiveness in cart page

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -11,8 +11,8 @@ const Cart = () => {
 
 
     return (
-        <main className='w-10/12 mx-auto mt-20 flex flex-row-reverse justify-center gap-16'>
-            <aside className='w-3/12 max-w-[500px] h-[50vh] flex flex-col gap-3'>
+        <main className='w-10/12 mx-auto mt-20 flex flex-col md:flex-row-reverse justify-center gap-16'>
+            <aside className='md:w-3/12 max-w-[500px] h-[50vh] flex flex-col gap-3'>
                 <h1 className='font-semibold text-black text-xl'>Order Details</h1>
                 <div className='w-full p-4 border rounded-md flex flex-col gap-2'>
                     <div className='flex flex-col gap-3 border-b pb-3 text-gray-500'>
@@ -36,7 +36,7 @@ const Cart = () => {
                 </div>
                 <button className="btn bg-black text-white w-full">GO TO CHECKOUT</button>
             </aside>
-          <section className='w-9/12'>
+          <section className='md:w-9/12'>
            {amount===0? 
            <div className='w-full flex justify-center items-center h-[50vh]'>
                     <h1 className='text-xl font-semibold'>You haven't added any items to cart </h1>
@@ -45,18 +45,18 @@ const Cart = () => {
             <h1 className='text-xl font-semibold'>An overview of your order</h1>
           <div className='w-full grid grid-cols-1 bg-base-200'>
                 {cart?.map(item=>{
-                    return <div className="flex h-[200px] w-full justify-between p-5 rounded-md border-b">
-                   <div className='flex gap-4 '>
-                   <div className='flex gap-4 items-center'>
-                    <div className='border flex items-center p-2 h-fit rounded-md gap-2 '>
+                    return <div className="w-full flex lg:h-[200px]  justify-between p-5 rounded-md border-b">
+                   <div className='flex gap-4 flex-col lg:flex-row'>
+                   <div className='flex gap-4 items-center flex-col md:flex-row w-full'>
+                    <div className=' border flex items-center p-2 h-fit rounded-md gap-2 '>
                     <LuMinus  role='button' onClick={()=> decrementItem(item)}/>
                         <span>{item.amount}</span>
                     <GoPlus className='text-2xl' onClick={()=> addToCart(item)}  role='button'/>
                     </div>
-                   <figure className="w-[200px] h-full">
+                   <figure className="w-full max-w-[200px] h-full">
                       <img
                         src={item.img}
-                        className="rounded-xl w-[200px] h-full object-cover" />
+                        className="rounded-xl w-full  h-full object-cover" />
                     </figure>
                    </div>
                    <h1 className='w-full mt-3 font-bold '>{item.name}</h1>
